9dd963e4a64e21336de5872996f039edd326a1d3
infinity.ethereal <infinity.ethereal@9fa76b84-cec5-8abd-45b1-de927d2ca524>
2012-03-28 00:27:10 +0000

    [DCN Lab - Spring 2012] - Lab 09 improvements. TCP header analysis implemented.

--- trunk/DCN Lab - Spring 2012/Lab09_TCP_Header_Analysis/TCP_Header_Analysis/src/PacketManip.cpp
+++ trunk/DCN Lab - Spring 2012/Lab09_TCP_Header_Analysis/TCP_Header_Analysis/src/PacketManip.cpp
@@ -2,10 +2,12 @@
 #include <cstdlib>			// exit()
 #include <ctime>
 #include <iostream>
+#include <bitset>
 
 using std::cout;
 using std::cerr;
 using std::endl;
+using std::bitset;
 
 void packet_capture_callback(u_char *, const struct pcap_pkthdr*, const u_char*);
 
@@ -48,8 +50,107 @@ void CPacketManip::Loop ()
 	pcap_loop (descr, -1, packet_capture_callback, NULL);
 }
 
-// All the work needs to be done here.
+// TODO: All the work needs to be done here.
 void packet_capture_callback(u_char *useless,const struct pcap_pkthdr* header,const u_char* packet)
 {
+	// TODO: Comment this to stop some spam onscreen or set the appropriate filter program in main() if running on network.
 	cout << " Recieved a packet at: " << ctime((const time_t*)&header->ts.tv_sec) << endl;
+
+	// Header pointers.
+	const struct sniff_ethernet *ethernet; /* The ethernet header */
+	const struct sniff_ip *ip; /* The IP header */
+	const struct sniff_tcp *tcp; /* The TCP header */
+	const u_char *payload; /* Packet payload */
+
+	u_int size_ip;		// IP header length.
+	u_int size_tcp;		// TCP header legnth.
+
+	// ************************** Pointer Initialization *********************************
+	// packet* is the starting address of captured packet stored in memory.
+	ethernet = (struct sniff_ethernet*)(packet);			// First thing in a packet is ethernet header.
+	ip = (struct sniff_ip*)(packet + SIZE_ETHERNET);		// IP header comes after ethernet header.
+
+	size_ip = IP_HL(ip)*4;		// IP_HL is a macro for ((ip)->ip_vhl) & 0x0f;
+								// IP_HL(ip) gives length in 32bit words. Multiplication by 4 gives length in bytes.
+	if (size_ip < 20)
+	{		
+		cerr << "ERROR: Invalid IP header length: " << size_ip << " bytes." << endl;
+		return;
+	}
+
+	tcp = (struct sniff_tcp*)(packet + SIZE_ETHERNET + size_ip);		// TCP header follows ethernet and IP headers.
+	size_tcp = TH_OFF(tcp)*4;	// TH_OFF(tcp) is a macro for ((ip)->ip_vhl) >> 4;
+								// TH_OFF(tcp) gives length of TCP header in 32bit words. Multiplication by 4 gives length in bytes.
+	if (size_tcp < 20)
+	{
+		cerr << "ERROR: Invalid TCP header length: " << size_tcp << " bytes." << endl;
+		return;
+	}
+	
+	payload = (u_char *)(packet + SIZE_ETHERNET + size_ip + size_tcp);	// Payload or data in packet.
+	// *************************************************************************************
+
+	
+	// TODO: Packet capturing condition. Only process packets with the specified protocol, and port numbers.
+	// TODO: Process UDP packets and display their headers.
+	// Default condition is to capture only packets with TCP protocol and source port = 6000 or 6001
+	// ntohs(tcp->th_sport) gives source port
+	// ntohs(tcp->th_dport) gives destination port
+	// Protocols are IPPROTO_IP, IPPROTO_UDP, IPPROTO_ICMP etc.
+	if ( ip->ip_p == IPPROTO_TCP && (ntohs(tcp->th_sport) == 6000 || ntohs(tcp->th_sport) == 6001) )
+	{
+		// TODO: Process the headers. Display IP header.
+		cout << " Recieved a TCP packet at: " << ctime((const time_t*)&header->ts.tv_sec) << endl;
+
+		/* define/compute tcp payload (segment) offset */
+		//payload = (packet + SIZE_ETHERNET + size_ip + size_tcp);
+		/* compute tcp payload (segment) size */
+		u_int size_payload = ntohs(ip->ip_len) - (size_ip + size_tcp);
+		u_int PacketSize = size_payload + size_ip + SIZE_ETHERNET + size_tcp;
+		cout << "Payload size: " << size_payload << endl;
+		cout << "Packet size: " << PacketSize << endl;
+
+		// bitset is a C++ class for bit manipulation. It has overloaded [], << and >> operators for easy access to individual bits and input/output.
+		// Using bitset of size 8 to display bits in a single character (byte).
+		bitset<8> DataOffset;
+		bitset<8> Flags;
+		DataOffset = tcp->th_offx2;		// tcp->th_offx2 points to offset character.
+		Flags = tcp->th_flags;			// tcp->th_flags character contain flag bits.
+
+		cout << "******* TCP Header *******" << endl
+			 << "Source Port:         " << ntohs(tcp->th_sport) << endl
+			 << "Destination Port:    " << ntohs(tcp->th_dport) << endl
+			 << "Sequence No.:        " << ntohl (tcp->th_seq) << endl
+			 << "Acknowledgement No.: " << ntohl(tcp->th_ack) << endl
+			 << "TCP header length:   " << size_tcp << endl
+			 << "Data offset flags:   " << DataOffset << endl
+			 << "Flags:               " << endl
+										<< "\t\t" << "FIN:    " << Flags[0] << endl
+										<< "\t\t" << "SYN:    " << Flags[1] << endl
+										<< "\t\t" << "RESET:  " << Flags[2] << endl
+										<< "\t\t" << "PUSH:   " << Flags[3] << endl
+										<< "\t\t" << "ACK:    " << Flags[4] << endl
+										<< "\t\t" << "URGENT: " << Flags[5] << endl
+										<< "\t\t" << "ECE:    " << Flags[6] << endl
+										<< "\t\t" << "CWR:    " << Flags[7] << endl
+			 << "Window size:         " << ntohs(tcp->th_win) << endl
+			 << "Checksum:            " << ntohs(tcp->th_sum) << endl
+			 << "Urgent Pointer:      " << ntohs(tcp->th_urp) << endl;
+
+		// TODO: Display payload data.
+		if (size_payload != 0)
+		{
+			cout << "Payload: ";
+			u_int Payload_Offset = SIZE_ETHERNET + size_ip + size_tcp;
+			for (int i=0; i<size_payload; i++)
+			{
+				cout << packet[i + Payload_Offset];
+			}
+			cout << endl;
+		}
+		cout << "*************************" << endl;
+		
+	}
+	else
+		return;		// Not a packet of our interest.
 }
--- trunk/DCN Lab - Spring 2012/Lab09_TCP_Header_Analysis/TCP_Header_Analysis/src/TCP_Header_Analysis.cpp
+++ trunk/DCN Lab - Spring 2012/Lab09_TCP_Header_Analysis/TCP_Header_Analysis/src/TCP_Header_Analysis.cpp
@@ -5,12 +5,15 @@ using std::endl;
 
 int main (int argc, char **argv)
 {
-	cout << "Automated Worm Fingerprinting." << endl;
+	cout << "TCP Header Analysis v1.00." << endl;
 
 	// Empty filter program will capture all packets on device.
 	// set device as "lo" to capture packets on loopback interface.
 	// filter program can be "dst localhost", "src localhost", "dst www.google.com" etc.
-	CPacketManip PacketCapture ("eth0", "");
+	// Empty string "" captures all packets.
+	// TODO: Is this running on localhost or on network? Use loopback for localhost (single PC) and eth0 for network.
+	//CPacketManip PacketCapture ("eth0", "");
+	CPacketManip PacketCapture ("lo", "");
 
 	PacketCapture.Loop ();
 	return 0;
