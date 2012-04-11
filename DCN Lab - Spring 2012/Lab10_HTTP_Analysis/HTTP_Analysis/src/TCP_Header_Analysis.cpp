36d34b759c820ff9a1a4a375c7ffac2878e914e0
infinity.ethereal <infinity.ethereal@9fa76b84-cec5-8abd-45b1-de927d2ca524>
2012-04-11 05:03:46 +0000

    [DCN Lab - Spring 2012] - Added Lab10_HTTP_Analysis.

---
+++ DCN Lab - Spring 2012/Lab10_HTTP_Analysis/HTTP_Analysis/src/TCP_Header_Analysis.cpp
@@ -0,0 +1,20 @@
+#include <PacketManip.h>
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main (int argc, char **argv)
+{
+	cout << "TCP Header Analysis v1.00." << endl;
+
+	// Empty filter program will capture all packets on device.
+	// set device as "lo" to capture packets on loopback interface.
+	// filter program can be "dst localhost", "src localhost", "dst www.google.com" etc.
+	// Empty string "" captures all packets.
+	// TODO: Is this running on localhost or on network? Use loopback for localhost (single PC) and eth0 for network.
+	//CPacketManip PacketCapture ("eth0", "");
+	CPacketManip PacketCapture ("eth0", "dst 172.16.100.98 || src 172.16.100.98");
+
+	PacketCapture.Loop ();
+	return 0;
+}
