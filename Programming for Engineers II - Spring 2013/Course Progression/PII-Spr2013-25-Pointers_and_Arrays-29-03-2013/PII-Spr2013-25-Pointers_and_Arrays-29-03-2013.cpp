c7259b977de3f37c922be8d61f48f732766d6ed0
infinity.ethereal <infinity.ethereal@9fa76b84-cec5-8abd-45b1-de927d2ca524>
2013-03-30 18:30:00 +0000

    [Programming for Engineers II - Spring 2013] - Update .cpp file for class 25.

--- Programming for Engineers II - Spring 2013/Course Progression/PII-Spr2013-25-Pointers_and_Arrays-29-03-2013/PII-Spr2013-25-Pointers_and_Arrays-29-03-2013.cpp
+++ Programming for Engineers II - Spring 2013/Course Progression/PII-Spr2013-25-Pointers_and_Arrays-29-03-2013/PII-Spr2013-25-Pointers_and_Arrays-29-03-2013.cpp
@@ -1,106 +1,126 @@
-\begin{lstlisting}[caption={Pointer Basics}]
+\begin{lstlisting}[caption={Pointers to Objects}]
 #include <iostream>
 using namespace std;
+
+class Complex
+{
+	public:
+	float real, img;
+};
 int main()
 {
-	int x = 3; // x declaration and initialisation.
+	Complex* c;
+	delete c; // What will this do? Crash! Don't try to de-allocate un-allocated pointers.
+
+	c = new Complex; // Dynamic memory allocation.
+
+	// Member access using arrow operator.
+	c->real = 3; // Also, (*c).real
+	c->img = 5;  // Also, (*c).img
+
 	int* px; // Pointer declaration.
 	px = &x; // Pointer xp now `points' to x.
-	
-	cout << x << endl;   // Value of x.
-	cout << &x << endl;  // Address of x.
-	cout << xp << endl;  // Value of xp = Address of x.
-	cout << *xp << endl; // Pointer dereference: Using xp to access value of x.
-	
-	return 0;
-}
-\end{lstlisting}
-\begin{lstlisting}[caption={Dynamic memory allocation with pointer}]
-int main()
-{
-	int* p;
-	
-	// Single integer.
-	p = new int; // Allocation.
-	*p = 3;
-	delete p;    // De-allocation.
-	
-	// Integer array.
-	p = new int[5]; // Array allocation.
-	for (int i=0; i<5; i++)
-	{
-		p[i] = i;
-	}		
-	delete[] p;     // Array de-allocation.
-	
-	return 0;
-}
-\end{lstlisting}
-\begin{lstlisting}[caption={Default object copy}]
-#include <iostream>
-using namespace std;
-class Test
-{
-	public:
-	int x;
-};
-int main()
-{
-	Test A, B;
-	A.x = 3;
-	
-	B = A;		// Assignment operator.
-	Test C = A; // Copy constructor.
-	
-	// B.x and C.x now contain 3. Same as A.x.
-	// Changing B.x won't affect A.x or B.x.
-	// All x's are independent.
-	
-	return 0;
-}
-\end{lstlisting}
-\begin{lstlisting}[caption={Default cbject copy with pointers}]
-#include <iostream>
-using namespace std;
-class Test
-{
-	public:
-	int* x;
-};
-int main()
-{
-	Test A, B;
-	A.x = new int;
-	*(A.x) = 3;
-	
-	B = A;		// Assignment operator.
-	Test C = A; // Copy constructor.
-	
-	delete A.x; // De-allocates memory for all pointers!
 
-	// B.x and C.x now contain the same address as A.x.
-	// All point to same memory location!
-	// Sharing same value!
-	
+	delete c; // De-allocation.
+
+	// Array allocation.
+	c = new Complex[10];
+
+	for (int i=0; i<10; i++)
+	{
+		c[i].real = 3;
+		c[i].img = 5;
+	}
+	delete[] c; // Release or de-allocate memory.
+
 	return 0;
 }
 \end{lstlisting}
-\begin{lstlisting}[caption={Overloading copy constructor and assignment operator}]
-class Test
+
+\begin{lstlisting}[caption={Pointer levels}]
+#include <iostream>
+using namespace std;
+
+int main()
 {
-	public:
-	int* x;
-	// Copy constructor.
-	Test(const Test& rhs)
+	int** ipp;
+
+	int x1 = 5;
+	int x2 = 6;
+	int x3 = 7;
+
+	int* ip1;
+	int* ip2;
+
+	ip1 = &x1;
+	ip2 = &x2;
+	ipp = &ip1;
+
+	*ipp = ip2;
+	*ipp = &x3;
+	**ipp = -1;
+
+	return 0;
+}
+\end{lstlisting}
+
+\begin{lstlisting}[caption={Arrays and Pointers}]
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int Array[5] = {3,7,2,1,5};
+
+	cout << Array << endl; // Prints the address of array.
+
+	// Using subscript.
+	cout << Array[0] << endl; // Print the value of element located a distance of 0 from start.
+	cout << Array[3] << endl; // Print the value of element located a distance of 3 from start.
+	cout << Array[5] << endl; // Print the value of element located a distance of 5 from start. ERROR! Out of bound.
+
+	// Using Pointers.
+	cout << *(Array+0) << endl; // Prints the value of first element which is 3.
+	cout << *(Array+1) << endl; // Prints the value of second element which is 7.
+	cout << *(Array+5) << endl; // Prints the value of sixth element which is out of bound. ERROR!
+
+	return 0;
+}
+\end{lstlisting}
+
+\begin{lstlisting}[caption={Dynamic 2D array}]
+int main()
+{
+	int Rows = 3;
+	int Cols = 3;
+	// Pointer.
+	int** Array2D;
+
+	// Array of int* 'ers equal to number of rows.
+	Array2D = new int*[Rows];
+	// Each int* is used to allocate int array equal to number of columns.
+	for (int i=0; i<Rows; i++)
 	{
-		x = new int;
-		*x = *(rhs.x); // Copying value.
+		Array2D[i] = new int[Cols];
 	}
-	// Assignment operator.
-	Test operator= (const Test& rhs)
+
+	// Accessing array elements.
+	for (int i=0; i<Rows; i++)
 	{
-		x = new int;
-		*x = *(rhs.x); // Copying value.
+		for (int j=0; j<Cols; j++)
+		{
+			Array2D[i][j] = i*j;
+		}
 	}
-};
+	// Array2D[i][j] = *((*(Array2D+i))+j) = (*(Array2D+i))[j] = *(Array2D[i]+j)
+
+	// De-allocation
+	for (int i=0; i<Rows; i++)
+	{
+		delete[] Array2D[i];
+	}
+	delete[] Array2D;
+
+	return 0;
+}
 \end{lstlisting}
