92ab25749894fdaf00ec6b4c647315a84c81d6d0
infinity.ethereal <infinity.ethereal@9fa76b84-cec5-8abd-45b1-de927d2ca524>
2013-03-31 16:08:38 +0000

    [Programming for Engineers II - Spring 2013] - Update class 26 on advanced pointer topics.

--- trunk/Programming for Engineers II - Spring 2013/Course Progression/PII-Spr2013-26-Advanced_Pointers_and_Keyword_this-01-04-2013/PII-Spr2013-26-Advanced_Pointers_and_Keyword_this-01-04-2013.cpp
+++ trunk/Programming for Engineers II - Spring 2013/Course Progression/PII-Spr2013-26-Advanced_Pointers_and_Keyword_this-01-04-2013/PII-Spr2013-26-Advanced_Pointers_and_Keyword_this-01-04-2013.cpp
@@ -1,106 +1,170 @@
-\begin{lstlisting}[caption={Pointer Basics}]
+\begin{lstlisting}[caption={Pointer Example},escapechar=!]
 #include <iostream>
 using namespace std;
-int main()
-{
-	int x = 3; // x declaration and initialisation.
-	int* px; // Pointer declaration.
-	px = &x; // Pointer xp now `points' to x.
-	
-	cout << x << endl;   // Value of x.
-	cout << &x << endl;  // Address of x.
-	cout << xp << endl;  // Value of xp = Address of x.
-	cout << *xp << endl; // Pointer dereference: Using xp to access value of x.
-	
-	return 0;
-}
-\end{lstlisting}
-\begin{lstlisting}[caption={Dynamic memory allocation with pointer}]
-int main()
-{
-	int* p;
-	
-	// Single integer.
-	p = new int; // Allocation.
-	*p = 3;
-	delete p;    // De-allocation.
-	
-	// Integer array.
-	p = new int[5]; // Array allocation.
-	for (int i=0; i<5; i++)
-	{
-		p[i] = i;
-	}		
-	delete[] p;     // Array de-allocation.
-	
-	return 0;
-}
-\end{lstlisting}
-\begin{lstlisting}[caption={Default object copy}]
-#include <iostream>
-using namespace std;
-class Test
-{
-	public:
-	int x;
-};
-int main()
-{
-	Test A, B;
-	A.x = 3;
-	
-	B = A;		// Assignment operator.
-	Test C = A; // Copy constructor.
-	
-	// B.x and C.x now contain 3. Same as A.x.
-	// Changing B.x won't affect A.x or B.x.
-	// All x's are independent.
-	
-	return 0;
-}
-\end{lstlisting}
-\begin{lstlisting}[caption={Default cbject copy with pointers}]
-#include <iostream>
-using namespace std;
-class Test
-{
-	public:
-	int* x;
-};
-int main()
-{
-	Test A, B;
-	A.x = new int;
-	*(A.x) = 3;
-	
-	B = A;		// Assignment operator.
-	Test C = A; // Copy constructor.
-	
-	delete A.x; // De-allocates memory for all pointers!
 
-	// B.x and C.x now contain the same address as A.x.
-	// All point to same memory location!
-	// Sharing same value!
-	
+int main()
+{
+	int** pp;
+	int* p1;
+	int* p2;
+
+	arr1[5] = {6,7,8,9,10};
+	arr2[5] = {1,2,3,4,5};
+
+	p1 = arr1;
+	p2 = arr2;
+	pp = &p1;
+
+	cout << "&p1: " << &p1 << endl
+		 << "p1: " << p1 << endl
+		 << "*p1: " << *p1 << endl
+		 << "*(p1+0): " << *(p1+0) << endl
+		 << "p1[0]: " << p1[0] << endl
+		 << "p1[1]: " << p1[1] << endl
+		 << "*(p1+2): " << *(p1+2) << endl;
+
+	cout << "&pp: " << &pp << endl
+		 << "pp: " << pp << endl
+		 << "*pp: " << *pp << endl
+		 << "**pp: " << **pp << endl
+		 << "*(pp+0): " << *(pp+0) << endl
+		 << "pp[0]: " << pp[0] << endl
+		 << "*pp[0]: " << *pp[0] << endl
+		 << "(*pp)[0]: " << (*pp)[0] << endl
+		 << "*(pp[0]): " << *(pp[0]) << endl
+		 << "pp[1]: " << pp[1] << endl
+		 << "*pp[1]: " << *pp[1] << endl
+		 << "(*pp)[1]: " << (*pp)[1] << endl
+		 << "*(pp[1]): " << *(pp[1]) << endl;
+
 	return 0;
 }
 \end{lstlisting}
-\begin{lstlisting}[caption={Overloading copy constructor and assignment operator}]
-class Test
+
+\begin{lstlisting}[caption={\texttt{this} pointer}]
+#include <iostream>
+using namespace std;
+
+class Complex
 {
+	private:
+	float real;
+	float img;
 	public:
-	int* x;
-	// Copy constructor.
-	Test(const Test& rhs)
+	Complex()
 	{
-		x = new int;
-		*x = *(rhs.x); // Copying value.
+		cout << "My object's address is: " << this << endl;
 	}
-	// Assignment operator.
-	Test operator= (const Test& rhs)
+	void Input()
 	{
-		x = new int;
-		*x = *(rhs.x); // Copying value.
+		cin >> this->real;
+		cin >> this->img;
+	}
+	void Display()
+	{
+		cout << this->real << endl;
+		cout << this->img << endl;
 	}
 };
+
+int main()
+{
+	Complex c;
+	c.Input();
+	c.Display();
+
+	return 0;
+}
+\end{lstlisting}
+
+\begin{lstlisting}[caption={Returning by reference using \texttt{this} pointer},escapechar=!]
+class Complex
+{
+	private:
+	float real;
+	float img;
+	public:
+	Test& operator= (Test & rhs)
+	{
+		real = rhs.real;
+		img = rhs.img;
+		return (*this);
+	}
+};
+int main()
+{
+	Complex A, B, C;
+	!\color{red}{A = B = C}!;
+
+	return 0;
+}
+\end{lstlisting}
+
+\begin{lstlisting}[caption={\texttt{void*} usage}]
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	void* vp;
+	vp = new int[5];
+	delete[] vp;
+
+	vp = new float;
+	*((float*)vp) = 3.3f; // Notice, explicit casting from void* to float* before dereferencing.
+	cout << "*vp: " << *((float*)vp) << endl;
+	delete vp;
+
+	return 0;
+}
+\end{lstlisting}
+
+\begin{lstlisting}[caption={constant string}]
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char str1[] = "Defined as an array";
+	char* str2 = "Defined as a pointer";
+	cout << str1 << endl; // display both strings
+	cout << str2 << endl;
+	//str1++; //can’t do this; str1 is a const ptr
+	str2++; // this is OK, str2 is a pointer
+	cout << str2 << endl; //now str2 starts "efined..."
+
+	return 0;
+}
+\end{lstlisting}
+
+\begin{lstlisting}[caption={Array of constant strings, contiguous storage}]
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int DAYS = 7;
+	char* arrptrs[DAYS] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+	for(int j=0; j<DAYS; j++) //cout every string
+		cout << arrptrs[j] << endl;
+
+	return 0;
+}
+\end{lstlisting}
+
+\begin{lstlisting}[caption={Constant column length, non-contiguous storage},escapechar=!]
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int DAYS = 7;
+	!\color{red}{char arrptrs[DAYS][10]}! = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+	for(int j=0; j<DAYS; j++) //cout every string
+		cout << arrptrs[j] << endl;
+
+	return 0;
+}
 \end{lstlisting}
