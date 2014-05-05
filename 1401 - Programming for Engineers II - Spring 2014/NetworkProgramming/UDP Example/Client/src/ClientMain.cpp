c95cdd4a83f2d148582cf2036b08cb00b15a0363
infinity.ethereal <infinity.ethereal@9fa76b84-cec5-8abd-45b1-de927d2ca524>
2014-05-05 13:46:30 +0000

    [1401 - Programming for Engineers II - Spring 2014] - Added TCP and UDP examples.

---
+++ 1401 - Programming for Engineers II - Spring 2014/NetworkProgramming/UDP Example/Client/src/ClientMain.cpp
@@ -0,0 +1,36 @@
+#include <Client.h>
+#include <cstdlib>			// exit(), atoi()
+#include <cstring>			// strlen()
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	// Create Client object.
+	Client ClientObj(UDPSOCKET);
+
+	ClientObj.CreateSocket(UDPSOCKET);
+	ClientObj.InitialiseAddress(6001);
+	ClientObj.Bind();
+
+	char ServerAddress[24];
+	int ServerPort;
+	cout << "Enter Server name or IP address: "; // Use localhost or 127.0.0.1 for local server.
+	cin.getline(ServerAddress, 24);
+	cout << "Server port: ";
+	cin >> ServerPort;
+
+	char UDPmessage[] = "UDP message from client.";
+	ClientObj.SendTo((void *)UDPmessage, (unsigned int)strlen (UDPmessage), ServerAddress, ServerPort);
+
+	cout << "Client recv'in from() packets..." << endl;
+	ClientObj.RecvFrom();
+	cout << "They say: " << ClientObj.GetBuffer() << endl;
+	ClientObj.DisplayTheirInfo();
+
+	ClientObj.CloseClientSocket();
+
+	return 0;
+}
