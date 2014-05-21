caf85119f7bd73f49815d660aa98298ec520dbff
infinity.ethereal <infinity.ethereal@9fa76b84-cec5-8abd-45b1-de927d2ca524>
2014-05-21 17:27:56 +0000

    [Programming for Engineers II - Spring 2014] NetworkProgramming: - Added TCP Asynchronous File Transfer. - Cleanup to Networking projects: Standardised directories and updated Makefiles.

---
+++ 1401 - Programming for Engineers II - Spring 2014/NetworkProgramming/TCP Asynchronous File Transfer/Client/src/ClientMain.cpp
@@ -0,0 +1,78 @@
+#include <Client.h>
+#include <cstdlib>			// exit(), atoi()
+#include <cstring>			// strlen()
+#include <string>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+#define RECVBUFFERSIZE 4096
+
+int main()
+{
+	// Create Client object.
+	Client ClientObj(TCPSOCKET, DEFAULTCLIENTPORT);			// Can create an object without arguments. Defaults are TCPSOCKET and DEFAULTPORT (6001).
+
+	// Create Client Socket.
+	ClientObj.CreateSocket(TCPSOCKET);
+	ClientObj.SetSocketOptions();
+
+	// Initialise and bind Client address.
+	ClientObj.InitialiseAddress(DEFAULTCLIENTPORT);	// Without any argument default port will be chosen.
+	ClientObj.Bind();
+
+	char ServerName[24];
+	int ServerPort;
+	cout << "Enter Server name or IP: "; // Use localhost or 127.0.0.1 for local server.
+	cin.getline(ServerName, 24);
+	cout << "Enter Server port: ";
+	cin >> ServerPort;
+
+	// Connect to Server. Server name/IP and port are provided as arguments.
+	ClientObj.Connect(ServerName, ServerPort);
+
+	// ********************** Actual File Transfer **********************
+	char Filename[40];
+	cout << "Enter filename to download from server (STL.pdf): ";
+	if (cin.peek() == '\n') cin.ignore(); // If input skips.
+	cin.getline(Filename, 40);
+
+	// 1. Send filename to server.
+	ClientObj.Send((void*)Filename, (unsigned int)strlen(Filename));
+	ClientObj.Receive(); // Receive ACK or NACK
+
+	// 2. Checking if file opened.
+	if (!strcmp(ClientObj.GetBuffer(), "NACK"))
+	{
+		cout << "Error: opening file." << endl;
+		exit(0);
+	}
+	else
+		ClientObj.Send((void*)"ACK", (unsigned int)strlen("ACK")); // Send ACK.
+
+	// 3. Getting filesize.
+	ClientObj.Receive();
+	int FileSize = *((int*)ClientObj.GetBuffer());
+	cout << "Filesize is " << FileSize << "B (" << (double)FileSize/1024 << "KB, " << (double)FileSize/1024/1024 << "MB)." << endl;
+	ClientObj.Send((void*)"ACK", sizeof("ACK")); // Sending ACK.
+
+	// 4. Receiving file in a loop.
+	fstream File;
+	string Received = "Received_";
+	Received = Received + Filename;
+	File.open(Received.c_str(), ios::out|ios::binary|ios::trunc);
+	int TotalBytesReceived = 0;
+	char FileBuffer[RECVBUFFERSIZE];
+	while (TotalBytesReceived != FileSize)
+	{
+		int BytesReceived = ClientObj.Receive((void*)FileBuffer, RECVBUFFERSIZE);
+		File.write(FileBuffer, BytesReceived);
+		TotalBytesReceived = TotalBytesReceived + BytesReceived;
+	}
+	File.close();
+	cout << "File received." << endl; // Received file is in 'Client' folder.
+
+	ClientObj.CloseClientSocket();
+
+	return 0;
+}
---
+++ 1401 - Programming for Engineers II - Spring 2014/NetworkProgramming/TCP Asynchronous File Transfer/Server/src/ServerMain.cpp
@@ -0,0 +1,89 @@
+#include <Server.h>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+using namespace std;
+
+#define SENDBUFFERSIZE 4096
+
+int main()
+{
+	// Create Server object.
+	Server ServerObj(TCPSOCKET, DEFAULTSERVERPORT);			// Without any arguments Server will set defaults, TCPSOCKET as type and 6000 as port.
+
+	// Create Server socket and set socket options.
+	ServerObj.CreateSocket(TCPSOCKET);		// No argument means TCPSOCKET
+	ServerObj.SetSocketOptions();
+
+	// Initialise Server address struct and bind it with Server's socket.
+	ServerObj.InitialiseAddress(6000);		// No argument here will take default port.
+	ServerObj.Bind();
+
+	// Listen for incoming connections.
+	ServerObj.Listen();
+	ServerObj.DisplayServerInfo();
+
+	// Accept any incoming connections.
+	ServerObj.Accept();
+	ServerObj.DisplayClientInfo();
+
+	// ***************** Actual File Transfer *****************
+	// 1. Receive the name of file. File should exist in the 'Server' folder.
+	cout << "Server waiting for filename." << endl;
+	ServerObj.Receive();
+	cout << "Client requested file: " << ServerObj.GetBuffer() << endl;
+
+	// 2. Open the file for reading.
+	fstream File;
+	File.open(ServerObj.GetBuffer(), ios::in|ios::binary);
+	if (File.is_open()==false)
+	{
+		cout << "Error: File failed to open" << endl;
+		ServerObj.Send((void*)"NACK", sizeof("NACK")); // Sending NACK if file failed to open.
+		exit(0);
+	}
+	else
+	{
+		ServerObj.Send((void*)"ACK", sizeof("ACK")); // Sending ACK if file opened.
+		ServerObj.Receive(); // Receive ACK from client.
+	}
+
+	// 3. Send file size to client.
+	File.seekg(ios::beg, ios::end);
+	int FileSize = (int)File.tellg();
+	ServerObj.Send((void*)&FileSize, sizeof(FileSize));
+	ServerObj.Receive(); // Receive ACK (acknowledgement) from client.
+
+	// 4. Send file in a loop.
+	char FileBuffer[SENDBUFFERSIZE]; // Buffer to hold data read from file.
+
+	File.seekg(0);
+	int TotalBytesSent = 0;
+	while (TotalBytesSent != FileSize)
+	{
+		int BytesRead;
+		int BytesSent = 0;
+		
+		if (FileSize-TotalBytesSent >= SENDBUFFERSIZE)
+			BytesRead = SENDBUFFERSIZE;
+		else
+			BytesRead = FileSize-TotalBytesSent;
+
+		File.read(FileBuffer, BytesRead);
+		while (BytesSent != BytesRead)
+		{
+			BytesSent = ServerObj.Send((void*)(FileBuffer+BytesSent), BytesRead-BytesSent);
+			TotalBytesSent = TotalBytesSent + BytesSent;
+		}
+	}
+	File.close();
+	cout << "File transfered." << endl;
+	// ****************************************************************
+
+	// Close sockets.
+	ServerObj.CloseClientSocket();
+	ServerObj.CloseServerSocket();
+
+	return 0;
+}
