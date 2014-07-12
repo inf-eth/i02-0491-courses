26e7f225bbdaddb20d9f07d9e2be00dae78d1fcd
infinity.ethereal <infinity.ethereal@9fa76b84-cec5-8abd-45b1-de927d2ca524>
2014-07-12 06:32:39 +0000

    [1402 - Programming for Engineers I - Summer 2014] - Added sessional exam. [1402 - Microwave Engineering - Summer 2014] - Added course folder with sessional exam, course outline and transmission line simulation.

---
+++ trunk/1402 - Microwave Engineering - Summer 2014/TransmissionLineModelling/TransmissionLineModelling.m
@@ -0,0 +1,417 @@
+clc
+clear all
+
+% Simulation parameters.
+SIZE = 2000; % No. of spatial steps
+SlabLeft = 1800; % Location of left end of Slab.
+SlabRight = SlabLeft+50; % Location of right end of Slab
+MaxTime = 4*SIZE; % No. of time steps
+PulseWidth = round(SIZE/8); % Controls width of Gaussian Pulse
+td = PulseWidth; % Temporal delay in pulse.
+aimp = 20; % Impulse scaling parameter
+Amplitude = 2; % Square pulse and unit step amplitude
+source = 20; % Location of source
+SaveFields = 1; % 0. No, 1. Yes.
+SnapshotInterval = 32; % Amount of time delay between snaps.
+PECLoad = -1; % Location of PEC load
+PMCLoad = -1; % Location of PMC Load
+% Choice of source.
+% 1. Gaussian 2. Sine wave 3. Ricker wavelet 4. Square Pulse 5. Unit Step 6.
+% Impulse
+SourceChoice = 2;
+
+% Constants.
+c = 3e8;
+pi = 3.141592654;
+e0 = (1e-9)/(36*pi);
+u0 = (1e-7)*4*pi;
+
+dt = 1.0e-11/3;
+dz = 1.0e-3;
+Sc = c * dt/dz
+
+l = 0.5;
+f = c/l
+fmax = 1/(2*dt)
+w = 2*pi*f;
+k0 = w/c; % Free space wave number.
+% Ricker wavelet parameters.
+if SourceChoice == 3
+    fp = f; % Peak frequency
+    dr = PulseWidth*dt*2; % Delay
+end
+
+% Initialization.
+Ex = zeros(SIZE, 3); % x-component of E-field
+Dx = zeros(SIZE, 3); % x-component of D
+Hy = zeros(SIZE, 3); % y-component of H-field
+By = zeros(SIZE, 3); % y-component of B
+
+% Incident and Transmitted Fields.
+Exi = zeros(MaxTime, 1);
+Ext = zeros(MaxTime, 1);
+Extt = zeros(MaxTime, 1);
+x1 = SlabLeft+1; % Position of observation.
+
+% Refractive Index calculations.
+Z1 = SlabLeft + 50;
+z1 = Z1*dz;
+Z2 = SlabLeft + 60;
+z2 = Z2*dz;
+Exz1 = zeros(MaxTime, 1);
+Exz2 = zeros(MaxTime, 1);
+
+einf = ones(SIZE,1);
+einf(SlabLeft:SlabRight) = 4; % einf(Drude) or er in slab.
+uinf = ones(SIZE,1);
+uinf(SlabLeft:SlabRight) = 1; % uinf(Drude) or ur in slab.
+wpesq = zeros(SIZE,1);
+wpesq(SlabLeft:SlabRight) = 0*2*w^2; % DNG(Drude) value of wpe squared in slab.
+wpmsq = zeros(SIZE,1);
+wpmsq(SlabLeft:SlabRight) = 0*2*w^2; % DNG(Drude) value of wpm squared in slab.
+ge = zeros(SIZE,1);
+ge(SlabLeft:SlabRight) = 0*w/32; % Electric collision frequency in slab.
+gm = zeros(SIZE,1);
+gm(SlabLeft:SlabRight) = 0*w/32; % Magnetic collision frequency in slab.
+
+a0 = (4*dt^2)./(e0*(4*einf+dt^2*wpesq+2*dt*einf.*ge));
+a = (1/dt^2)*a0;
+b = (1/(2*dt))*ge.*a0;
+c = (e0/dt^2)*einf.*a0;
+d = (-1*e0/4).*wpesq.*a0;
+e = (1/(2*dt))*e0*einf.*ge.*a0;
+am0 = (4*dt^2)./(u0*(4*uinf+dt^2*wpmsq+2*dt*uinf.*gm));
+am = (1/dt^2)*am0;
+bm = (1/(2*dt))*gm.*am0;
+cm = (u0/dt^2)*uinf.*am0;
+dm = (-1*u0/4).*wpmsq.*am0;
+em = (1/(2*dt))*u0*uinf.*gm.*am0;
+
+if SaveFields == 1
+    ExSnapshots = zeros(SIZE, MaxTime/SnapshotInterval); % Data for plotting.
+    HySnapshots = zeros(SIZE, MaxTime/SnapshotInterval); % Data for plotting.
+    frame = 1;
+end
+
+n1 = 1;
+n2 = 2;
+% Outer loop for time-stepping.
+tic
+% % Test loop for incident field in free space.
+% for q = 0:MaxTime
+%     
+%     % Progress indicator.
+%     if mod(q,SnapshotInterval) == 0
+%         fprintf(1, repmat('\b',1,linecount));
+%         linecount = fprintf(1, '%g %%', (q*100)/MaxTime );
+%     end
+%     
+%     % Calculation of Hy using update difference equation for Hy. This is time step q.
+%     Hy(1:SIZE-1,n2) = Hy(1:SIZE-1,n1) + ( ( Ex(1:SIZE-1,n1) - Ex(2:SIZE,n1) ) * dt/(u0*dz) );
+%         
+%     % ABC for H at SIZE.
+%     Hy(SIZE,n2) = Hy(SIZE-1,n1) + (Sc-1)/(Sc+1)*(Hy(SIZE-1,n2) - Hy(SIZE,n1) );
+%  
+%     % Calculation of Ex using updated difference equation for Ex. This is time step q+1/2.
+%     Ex(2:SIZE,n2) = Ex(2:SIZE, n1) + ( dt/(e0*dz)*(Hy(1:SIZE-1, n2) - Hy(2:SIZE, n2)) );
+%     
+%     % ABC for E at 1.
+%     Ex(1,n2) = Ex(2,n1) + (Sc-1)/(Sc+1)*(Ex(2,n2) - Ex(2,n1));
+%     
+%     % Source.
+%     if SourceChoice == 1
+%     Ex(source,n2) = Ex(source,n2) + exp( -1*((q-td)/(PulseWidth/4))^2 ) * Sc;
+%     elseif SourceChoice == 2
+%     Ex(source,n2) = Ex(source,n2) + sin(2*pi*f*(q)*dt) * Sc;
+%     elseif SourceChoice == 3
+%     Ex(source,n2) = Ex(source,n2) + (1-2*(pi*fp*(q*dt-dr))^2)*exp(-1*(pi*fp*(q*dt-dr))^2) * Sc;
+%     elseif SourceChoice == 4
+%     if q > td && q <= td+PulseWidth
+%         Ex(source,n2) = Ex(source,n2) + Amplitude * Sc;
+%     end
+%     elseif SourceChoice == 5
+%     Ex(source,n2) = Ex(source,n2) + Amplitude * Sc;
+%     elseif SourceChoice == 6
+%     Ex(source,n2) = Ex(source,n2) + 1/(aimp*sqrt(pi))*exp(-1*((q-td)/aimp)^2) * Sc;
+%     end
+%     
+%     Exi(q+1) = Ex(x1,n2); % Incident field is left of slab.
+%     
+%     temp = n1;
+%     n1 = n2;
+%     n2 = temp;
+% end
+% fprintf(1, repmat('\b',1,linecount));
+% fprintf ( 1, 'Dry run complete! \n');
+% Reinitialization of fields for actual simulation.
+Ex = zeros(SIZE, 3); % x-component of E-field
+Hy = zeros(SIZE, 3); % y-component of H-field
+linecount = 0;
+% Actual simulation with scatterer.
+fprintf ( 1, 'Simulation started... \n');
+for q = 0:MaxTime
+    
+    % Progress indicator.
+    if mod(q,SnapshotInterval) == 0
+        fprintf(1, repmat('\b',1,linecount));
+        linecount = fprintf(1, '%g %%', (q*100)/MaxTime );
+    end
+    
+    % Storing past fields.
+    Ex(:,3) = Ex(:,n2);
+    Dx(:,3) = Dx(:,n2);
+    Hy(:,3) = Hy(:,n2);
+    By(:,3) = By(:,n2);
+    
+    % Calculation of Hy using update difference equation for Hy. This is time step q.
+    By(1:SIZE-1,n2) = By(1:SIZE-1,n1) + ( ( Ex(1:SIZE-1,n1) - Ex(2:SIZE,n1) ) * dt/(dz) );
+    Hy(:,n2) = am.*(By(:,n2)-2*By(:,n1)+By(:,3))+bm.*(By(:,n2)-By(:,3))+cm.*(2*Hy(:,n1)-Hy(:,3))+dm.*(2*Hy(:,n1)+Hy(:,3))+em.*(Hy(:,3));
+    
+    if PMCLoad ~= -1
+        Hy(PMCLoad,n2) = 0;
+        By(PMCLoad,n2) = 0;
+    end
+    % ABC for H at SIZE.
+    Hy(SIZE,n2) = Hy(SIZE-1,n1) + (Sc-1)/(Sc+1)*(Hy(SIZE-1,n2) - Hy(SIZE,n1) );
+    By(SIZE,n2) = u0*Hy(SIZE,n2);
+
+    % Calculation of Ex using updated difference equation for Ex. This is time step q+1/2.
+    Dx(2:SIZE,n2) = Dx(2:SIZE, n1) + ( dt/(dz)*(Hy(1:SIZE-1, n2) - Hy(2:SIZE, n2)) );
+    Ex(:,n2) = a.*(Dx(:,n2)-2*Dx(:,n1)+Dx(:,3))+b.*(Dx(:,n2)-Dx(:,3))+c.*(2*Ex(:,n1)-Ex(:,3))+d.*(2*Ex(:,n1)+Ex(:,3))+e.*(Ex(:,3));
+    
+    % ABC for E at 1.
+    Ex(1,n2) = Ex(2,n1) + (Sc-1)/(Sc+1)*(Ex(2,n2) - Ex(2,n1));
+    Dx(1,n2) = e0*Ex(1,n2);
+    
+    % Source.
+    if SourceChoice == 1
+    Ex(source,n2) = Ex(source,n2) + Amplitude * exp( -1*((q-td)/(PulseWidth/4))^2 ) * Sc;
+    elseif SourceChoice == 2
+    Ex(source,n2) = Ex(source,n2) + Amplitude * sin(2*pi*f*(q)*dt) * Sc;
+    elseif SourceChoice == 3
+    Ex(source,n2) = Ex(source,n2) + Amplitude * (1-2*(pi*fp*(q*dt-dr))^2)*exp(-1*(pi*fp*(q*dt-dr))^2) * Sc;
+    elseif SourceChoice == 4
+    if q > td && q <= td+PulseWidth
+        Ex(source,n2) = Ex(source,n2) + Amplitude * Sc;
+    end
+    elseif SourceChoice == 5
+    Ex(source,n2) = Ex(source,n2) + Amplitude * Sc;
+    elseif SourceChoice == 6
+    Ex(source,n2) = Ex(source,n2) + 1/(aimp*sqrt(pi))*exp(-1*((q-td)/aimp)^2) * Sc;
+    end
+    Dx(source,n2) = e0*Ex(source,n2);
+
+    if (SaveFields == 1 && mod(q,SnapshotInterval) == 0)
+        ExSnapshots(:,frame) = Ex(:,n2);
+        HySnapshots(:,frame) = Hy(:,n2);
+        frame=frame+1;
+    end
+    
+    if PECLoad ~= -1
+        Ex(PECLoad,n2) = 0;
+        Dx(PECLoad,n2) = 0;
+    end
+    
+    Exi(q+1) = Ex(source,n2); % Incident field is left of slab.
+    Ext(q+1) = Ex(x1,n2);
+    Extt(q+1) = Ex(SlabRight+10,n2);
+    
+    % Fields for calculation of refractive index.
+    Exz1(q+1) = Ex(Z1, n2);
+    Exz2(q+1) = Ex(Z2, n2);
+    
+    temp = n1;
+    n1 = n2;
+    n2 = temp;
+end
+fprintf(1, repmat('\b',1,linecount));
+fprintf ( 1, 'Simulation complete! \n');
+toc
+% Postprocessing.
+Fs = 1/dt;                    % Sampling frequency
+T = dt;                       % Sample time
+L = length(Exi);              % Length of signal
+t = (0:L-1)*T;                % Time vector
+fspan = 100;                  % Points to plot in frequency domain
+
+figure(1)
+subplot(211)
+plot(Fs*t*dt, Exi, 'LineWidth', 2.0, 'Color', 'b')
+set(gca, 'FontSize', 10, 'FontWeight', 'b')
+title('Incident Field', 'FontSize', 12, 'FontWeight', 'b')
+xlabel('time', 'FontSize', 11, 'FontWeight', 'b')
+grid on
+figure(2)
+subplot(211)
+plot(Fs*t*dt, Ext, 'LineWidth', 2.0, 'Color', 'b')
+set(gca, 'FontSize', 10, 'FontWeight', 'b')
+title('Transmitted Field', 'FontSize', 12, 'FontWeight', 'b')
+xlabel('time', 'FontSize', 11, 'FontWeight', 'b')
+grid on
+figure(3)
+subplot(211)
+plot(Fs*t*dt, Extt, 'LineWidth', 2.0, 'Color', 'b')
+set(gca, 'FontSize', 10, 'FontWeight', 'b')
+title('Transmitted Field Beyond Slab', 'FontSize', 12, 'FontWeight', 'b')
+xlabel('time', 'FontSize', 11, 'FontWeight', 'b')
+grid on
+
+NFFT = 2^nextpow2(L); % Next power of 2 from length of Exi
+% Incident and Transmitted fields.
+EXI = fft(Exi,NFFT)/L;
+EXT = fft(Ext,NFFT)/L;
+EXTT = fft(Extt,NFFT)/L;
+% Refractive index calculations.
+EXZ1 = fft(Exz1,NFFT)/L;
+EXZ2 = fft(Exz2,NFFT)/L;
+f = Fs/2*linspace(0,1,NFFT/2+1);
+
+% Plot single-sided amplitude spectrum.
+figure(1)
+subplot(212)
+EXIp = 2*abs(EXI(1:NFFT/2+1));
+plot(f(1:fspan), EXIp(1:fspan), 'LineWidth', 2.0, 'Color', 'r')
+ylim([0 inf])
+set(gca, 'FontSize', 10, 'FontWeight', 'b')
+title('Single-Sided Amplitude Spectrum of Exi(t)', 'FontSize', 12, 'FontWeight', 'b')
+xlabel('Frequency (Hz)', 'FontSize', 11, 'FontWeight', 'b')
+ylabel('|EXI(f)|', 'FontSize', 11, 'FontWeight', 'b')
+grid on
+figure(2)
+subplot(212)
+EXTp = 2*abs(EXT(1:NFFT/2+1));
+plot(f(1:fspan), EXTp(1:fspan), 'LineWidth', 2.0, 'Color', 'r')
+ylim([0 inf])
+set(gca, 'FontSize', 10, 'FontWeight', 'b')
+title('Single-Sided Amplitude Spectrum of Ext(t)', 'FontSize', 12, 'FontWeight', 'b')
+xlabel('Frequency (Hz)', 'FontSize', 11, 'FontWeight', 'b')
+ylabel('|EXT(f)|', 'FontSize', 11, 'FontWeight', 'b')
+grid on
+figure(3)
+subplot(212)
+EXTTp = 2*abs(EXTT(1:NFFT/2+1));
+plot(f(1:fspan), EXTTp(1:fspan), 'LineWidth', 2.0, 'Color', 'r')
+ylim([0 inf])
+set(gca, 'FontSize', 10, 'FontWeight', 'b')
+title('Single-Sided Amplitude Spectrum of Extt(t)', 'FontSize', 12, 'FontWeight', 'b')
+xlabel('Frequency (Hz)', 'FontSize', 11, 'FontWeight', 'b')
+ylabel('|EXT(f)|', 'FontSize', 11, 'FontWeight', 'b')
+grid on
+
+% Transmission Coefficient.
+figure(4)
+subplot(211)
+TAU = abs(EXT(1:NFFT/2+1)./EXI(1:NFFT/2+1));
+plot(f(1:fspan), TAU(1:fspan), 'LineWidth', 2.0, 'Color', 'b')
+set(gca, 'FontSize', 10, 'FontWeight', 'b')
+title('Transmission Coefficient', 'FontSize', 12, 'FontWeight', 'b')
+xlabel('Frequency (Hz)', 'FontSize', 11, 'FontWeight', 'b')
+ylabel('|EXT(f)/EXI(f)|', 'FontSize', 11, 'FontWeight', 'b')
+axis([-1 1 -2 2])
+axis 'auto x'
+grid on
+subplot(212)
+plot(f(1:fspan), 1-TAU(1:fspan), 'LineWidth', 2.0, 'Color', 'b')
+set(gca, 'FontSize', 10, 'FontWeight', 'b')
+title('Reflection Coefficient', 'FontSize', 12, 'FontWeight', 'b')
+xlabel('Frequency (Hz)', 'FontSize', 11, 'FontWeight', 'b')
+ylabel('1-|EXT(f)/EXI(f)|', 'FontSize', 11, 'FontWeight', 'b')
+axis([-1 1 -2 2])
+axis 'auto x'
+grid on
+
+% Refractive Index calculations.
+nFDTD = (1/(1i*k0*(z1-z2))).*log(EXZ2(1:NFFT/2+1)./EXZ1(1:NFFT/2+1));
+figure(5)
+subplot(211)
+plot(f(1:fspan), real(nFDTD(1:fspan)), 'LineWidth', 2.0, 'Color', 'b');
+set(gca, 'FontSize', 10, 'FontWeight', 'b')
+title('Refractive index re(n)', 'FontSize', 12, 'FontWeight', 'b')
+xlabel('Frequency (Hz)', 'FontSize', 11)
+ylabel('re(n)', 'FontSize', 11)
+grid on
+subplot(212)
+plot(f(1:fspan), imag(nFDTD(1:fspan)), 'LineWidth', 2.0, 'Color', 'r');
+set(gca, 'FontSize', 10, 'FontWeight', 'b')
+title('Refractive index im(n)', 'FontSize', 12, 'FontWeight', 'b')
+xlabel('Frequency (Hz)', 'FontSize', 11, 'FontWeight', 'b')
+ylabel('im(n)', 'FontSize', 11, 'FontWeight', 'b')
+grid on
+
+figure (10)
+% Scatterer boundaries.
+hold off
+plot([dz*SlabLeft dz*SlabLeft], [-2 2], 'Color', 'r');
+hold on
+plot([dz*SlabRight dz*SlabRight], [-2 2], 'Color', 'r');
+linelength=1e-3*(SlabLeft-source);
+linex = -1*linelength:0.001:0;
+Z0 = 377;
+ZL = 377/sqrt(einf(SlabLeft))/sqrt(uinf(SlabLeft));
+Beta = l/(2*pi);
+Zin = Z0.*(ZL+i*Z0*tan(Beta.*linex))./(Z0+i*ZL*tan(Beta.*linex));
+plot(linex,Zin, 'LineWidth', 2.0, 'Color', 'b');
+set(gca, 'FontSize', 10, 'FontWeight', 'b')
+axis([0 dz*SIZE -377 377])
+title('Time Domain Simulation', 'FontSize', 12, 'FontWeight', 'b')
+xlabel('z-axis (meters)', 'FontSize', 11, 'FontWeight', 'b')
+ylabel('Input Impedance', 'FontSize', 11, 'FontWeight', 'b')
+grid on
+        
+if SaveFields == 1
+    % Simulation animation.
+    for i=1:frame-1
+        figure (6)
+        % Scatterer boundaries.
+        hold off
+        plot([dz*SlabLeft dz*SlabLeft], [-2 2], 'Color', 'r');
+        hold on
+        plot([dz*SlabRight dz*SlabRight], [-2 2], 'Color', 'r');
+        plot(dz*(1:length(ExSnapshots(:,i))),ExSnapshots(:,i), 'LineWidth', 2.0, 'Color', 'b');
+        set(gca, 'FontSize', 10, 'FontWeight', 'b')
+        axis([0 dz*SIZE -2 2])
+        title('Time Domain Simulation', 'FontSize', 12, 'FontWeight', 'b')
+        xlabel('z-axis (meters)', 'FontSize', 11, 'FontWeight', 'b')
+        ylabel('Electric field (Ex)', 'FontSize', 11, 'FontWeight', 'b')
+        grid on
+        figure (8)
+        % Scatterer boundaries.
+        hold off
+        plot([dz*SlabLeft dz*SlabLeft], [-2 2], 'Color', 'r');
+        hold on
+        plot([dz*SlabRight dz*SlabRight], [-2 2], 'Color', 'r');
+        plot(dz*(1:length(HySnapshots(:,i))),HySnapshots(:,i), 'LineWidth', 2.0, 'Color', 'b');
+        set(gca, 'FontSize', 10, 'FontWeight', 'b')
+        axis([0 dz*SIZE -2/377 2/377])
+        title('Time Domain Simulation', 'FontSize', 12, 'FontWeight', 'b')
+        xlabel('z-axis (meters)', 'FontSize', 11, 'FontWeight', 'b')
+        ylabel('Magnetic field (Hy)', 'FontSize', 11, 'FontWeight', 'b')
+        grid on
+%         figure (9)
+%         Scatterer boundaries.
+%         hold off
+%         plot([dz*SlabLeft dz*SlabLeft], [-2 2], 'Color', 'r');
+%         hold on
+%         plot([dz*SlabRight dz*SlabRight], [-2 2], 'Color', 'r');
+%         plot(dz*(1:length(HySnapshots(:,i))),ExSnapshots(:,i)./HySnapshots(:,i), 'LineWidth', 2.0, 'Color', 'b');
+%         set(gca, 'FontSize', 10, 'FontWeight', 'b')
+%         axis([0 dz*SIZE -377 377])
+%         title('Time Domain Simulation', 'FontSize', 12, 'FontWeight', 'b')
+%         xlabel('z-axis (meters)', 'FontSize', 11, 'FontWeight', 'b')
+%         ylabel('Input Impedance', 'FontSize', 11, 'FontWeight', 'b')
+%         grid on
+        % figure (7)
+        % Scatterer boundaries.
+%         hold off
+%         plot([SlabLeft SlabLeft], [-1 1], 'Color', 'r');
+%         hold on
+%         plot([SlabRight SlabRight], [-1 1], 'Color', 'r');
+%         plot((1:length(ExSnapshots(:,i))),ExSnapshots(:,i), 'LineWidth', 2.0, 'Color', 'b');
+%         set(gca, 'FontSize', 10, 'FontWeight', 'b')
+%         axis([0 SIZE -1 1])
+%         title('Time Domain Simulation', 'FontSize', 12, 'FontWeight', 'b')
+%         xlabel('Spatial step (k)', 'FontSize', 11, 'FontWeight', 'b')
+%         ylabel('Electric field (Ex)', 'FontSize', 11, 'FontWeight', 'b')
+%         grid on
+    end
+end
