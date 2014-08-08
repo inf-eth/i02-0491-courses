9a3c5a13561fdfbb88b15ece999607c459d10a76
infinity.ethereal <infinity.ethereal@9fa76b84-cec5-8abd-45b1-de927d2ca524>
2014-08-08 07:29:12 +0000

    [1402 - Programming for Engineers I - Summer 2014] - Added assignment 05 solution. - Forgot to add quiz 04 and 05.

---
+++ trunk/1402 - Programming for Engineers I - Summer 2014/Assignments/PI-Sum2014-Assignment-05-Solution/Client/src/ClientMain.cpp
@@ -0,0 +1,191 @@
+#include <Client.h>
+#include <cstdlib>			// exit(), atoi()
+#include <cstring>			// strlen()
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct StudentRecord
+{
+	char Name[40];
+	int Roll;
+	int Age;
+	float CGPA;
+	int CourseMarks[3];
+};
+
+void Input(StudentRecord&);
+void Display(StudentRecord&);
+
+int main()
+{
+	// Create Client object.
+	Client ClientObj;
+
+	// Create Client Socket.
+	ClientObj.CreateSocket(TCPSOCKET);
+	ClientObj.SetSocketOptions();
+
+	// Initialise and bind Client address.
+	ClientObj.InitialiseAddress(DEFAULTCLIENTPORT);	// Without any argument default port will be chosen.
+	ClientObj.Bind();
+
+	char ServerName[24];
+	int ServerPort;
+	cout << "Enter Server name or IP: "; // Use localhost or 127.0.0.1 for local server.
+	cin.getline(ServerName, 24);
+	cout << "Enter Server port: ";
+	cin >> ServerPort;
+
+	// Connect to Server. Server name/IP and port are provided as arguments.
+	ClientObj.Connect(ServerName, ServerPort);
+
+	// Authorisation
+	int a = 0;
+	char pass[20];
+	while (true)
+	{
+		cout << "Enter password: ";
+		if (cin.peek()=='\n') cin.ignore();
+		cin.getline(pass, 20);
+		ClientObj.Send((void*)pass, strlen(pass)+1);
+		ClientObj.Receive((void*)&a, sizeof(int));
+		if (a==1)
+		{
+			cout << "Successful login!" << endl;
+			break;
+		}
+		else
+			cout << "Incorrect password!" << endl;
+	}
+
+	// Send and receive.
+	StudentRecord temp;
+	int choice = -1;
+	char ReceiveBuffer[512];
+
+	while (choice != 6)
+	{
+		ClientObj.Receive((void*)ReceiveBuffer, 512);
+		cout << ReceiveBuffer << endl << ">>";
+		cin >> choice;
+		ClientObj.Send((void*)&choice, sizeof(int));
+
+		switch (choice)
+		{
+		case 1:
+			{
+				Input(temp);
+				ClientObj.Send((void*)&temp, sizeof(StudentRecord));
+				break;
+			}
+		case 2:
+			{
+				int NoOfRecords;
+				ClientObj.Receive((void*)&NoOfRecords, sizeof(int));
+				cout << "Total records: " << NoOfRecords << endl;
+				for (int i=0; i<NoOfRecords; i++)
+				{
+					ClientObj.Receive((void*)&temp, sizeof(StudentRecord));
+					cout << "===== Record No: " << i+1 << " =====" << endl;
+					Display(temp);
+				}
+				break;
+			}
+		case 3:
+			{
+				int found;
+				char SearchName[40];
+				cout << "Enter name to search: ";
+				if (cin.peek()=='\n') cin.ignore();
+				cin.getline(SearchName, 40);
+				ClientObj.Send((void*)SearchName, strlen(SearchName)+1);
+				ClientObj.Receive((void*)&found, sizeof(int));
+				if (found==1)
+				{
+					cout << "Record found!" << endl;
+					ClientObj.Receive((void*)&temp, sizeof(StudentRecord));
+					Display(temp);
+				}
+				else
+					cout << "Record was not found!" << endl;
+
+				break;
+			}
+		case 4:
+			{
+				int found;
+				char SearchName[40];
+				cout << "Enter name to search: ";
+				if (cin.peek()=='\n') cin.ignore();
+				cin.getline(SearchName, 40);
+				ClientObj.Send((void*)SearchName, strlen(SearchName)+1);
+				ClientObj.Receive((void*)&found, sizeof(int));
+				if (found==1)
+				{
+					cout << "Record found!" << endl;
+					cout << "Enter updated data: " << endl;
+					Input(temp);
+					ClientObj.Send((void*)&temp, sizeof(StudentRecord));
+					cout << "Record was updated!" << endl;
+				}
+				else
+					cout << "Record was not found!" << endl;
+
+				break;
+			}
+		case 5:
+			{
+				fstream File;
+				File.open("received_Course_Outline.pdf", ios::out|ios::binary);
+				int filesize;
+				ClientObj.Receive((void*)&filesize, sizeof(int));
+				cout << "Filesize: " << filesize << "B (" << (float)filesize/1024 << "KB)" << endl;
+				char c;
+				for (int i=0; i<filesize; i++)
+				{
+					ClientObj.Receive((void*)&c, sizeof(char));
+					File.write((char*)&c, sizeof(char));
+				}
+				File.close();
+				break;
+			}
+		case 6:
+			break;
+		}
+	}
+
+	ClientObj.CloseClientSocket();
+
+	return 0;
+}
+
+void Input(StudentRecord& S)
+{
+	cout << "Enter name: ";
+	if (cin.peek()=='\n') cin.ignore();
+	cin.getline(S.Name, 40);
+	cout << "Roll: ";
+	cin >> S.Roll;
+	cout << "Age: ";
+	cin >> S.Age;
+	cout << "CGPA: ";
+	cin >> S.CGPA;
+	cout << "Course 1 marks: ";
+	cin >> S.CourseMarks[0];
+	cout << "Course 2 marks: ";
+	cin >> S.CourseMarks[1];
+	cout << "Course 3 marks: ";
+	cin >> S.CourseMarks[2];
+}
+
+void Display(StudentRecord& S)
+{
+	cout << "Name: " << S.Name << endl;
+	cout << "Roll: " << S.Roll << endl;
+	cout << "Age:  " << S.Age << endl;
+	cout << "CGPA: " << S.CGPA << endl;
+	cout << "Course 1 Marks: " << S.CourseMarks[0] << endl;
+	cout << "Course 2 Marks: " << S.CourseMarks[1] << endl;
+	cout << "Course 3 Marks: " << S.CourseMarks[2] << endl;
+}
---
+++ trunk/1402 - Programming for Engineers I - Summer 2014/Assignments/PI-Sum2014-Assignment-05-Solution/Server/src/ServerMain.cpp
@@ -0,0 +1,184 @@
+#include <Server.h>
+#include <cstring>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct StudentRecord
+{
+	char Name[40];
+	int Roll;
+	int Age;
+	float CGPA;
+	int CourseMarks[3];
+};
+
+int main()
+{
+	// Create Server object.
+	Server ServerObj;
+
+	// Create Server socket and set socket options.
+	ServerObj.CreateSocket(TCPSOCKET);		// No argument means TCPSOCKET
+	ServerObj.SetSocketOptions();
+
+	// Initialise Server address struct and bind it with Server's socket.
+	ServerObj.InitialiseAddress(6000);		// No argument here will take default port.
+	ServerObj.Bind();
+
+	// Listen for incoming connections.
+	ServerObj.Listen();
+	ServerObj.DisplayServerInfo();
+
+	// Accept any incoming connections.
+	ServerObj.Accept();
+	ServerObj.DisplayClientInfo();
+
+	// Authorisation.
+	int a = 0;
+	char pass[] = "123";
+	char recvpass[20];
+	while (true)
+	{
+		ServerObj.Receive((void*)recvpass, 20);
+		if (strcmp(recvpass, pass)==0)
+			a = 1;
+		ServerObj.Send((void*)&a, sizeof(int));
+		if (a==1)
+			break;
+	}
+
+	// Send and receive.
+	StudentRecord temp;
+	fstream file;
+	int choice = -1;
+	char Menu[] = "1. Add record\n2. Display all records\n3. Search record\n4. Update record\n5. Download course outline\n6. Disconnect";
+	while (choice != 6)
+	{
+		ServerObj.Send((void*)Menu, strlen(Menu)+1); // Send NULL as well.
+		ServerObj.Receive((void*)&choice, sizeof(int));
+
+		switch (choice)
+		{
+		case 1:
+			{
+				ServerObj.Receive((void*)&temp, sizeof(StudentRecord));
+				file.open("records.rec", ios::out|ios::app|ios::binary);
+				file.write((char*)&temp, sizeof(StudentRecord));
+				file.close();
+				break;
+			}
+		case 2:
+			{
+				int NoOfRecords;
+				file.open("records.rec", ios::in|ios::binary);
+				if (file.is_open()==true)
+				{
+					file.seekg(0, ios::end);
+					NoOfRecords = (int)file.tellg()/sizeof(StudentRecord);
+				}
+				else
+					NoOfRecords = 0;
+
+				ServerObj.Send((void*)&NoOfRecords, sizeof(int));
+				file.seekg(0);
+				for (int i=0; i<NoOfRecords; i++)
+				{
+					file.read((char*)&temp, sizeof(StudentRecord));
+					ServerObj.Send((void*)&temp, sizeof(StudentRecord));
+				}
+				file.close();
+				break;
+			}
+		case 3:
+			{
+				int found = 0;
+				int index;
+				int NoOfRecords;
+				char SearchName[40];
+				ServerObj.Receive((void*)SearchName, 40);
+
+				file.open("records.rec", ios::in|ios::binary);
+				file.seekg(0, ios::end);
+				NoOfRecords = (int)file.tellg()/sizeof(StudentRecord);
+				file.seekg(0);
+				for (int i=0; i<NoOfRecords; i++)
+				{
+					file.read((char*)&temp, sizeof(StudentRecord));
+					if (strcmp(temp.Name, SearchName)==0)
+					{
+						found = 1;
+						index = i;
+						break;
+					}
+				}
+				ServerObj.Send((void*)&found, sizeof(int));
+				if (found==1)
+				{
+					file.seekg(index*sizeof(StudentRecord));
+					ServerObj.Send((void*)&temp, sizeof(StudentRecord));
+				}
+				file.close();
+				break;
+			}
+		case 4:
+			{
+				int found = 0;
+				int index;
+				int NoOfRecords;
+				char SearchName[40];
+				ServerObj.Receive((void*)SearchName, 40);
+
+				file.open("records.rec", ios::in|ios::out|ios::binary);
+				file.seekg(0, ios::end);
+				NoOfRecords = (int)file.tellg()/sizeof(StudentRecord);
+				file.seekg(0);
+				for (int i=0; i<NoOfRecords; i++)
+				{
+					file.read((char*)&temp, sizeof(StudentRecord));
+					if (strcmp(temp.Name, SearchName)==0)
+					{
+						found = 1;
+						index = i;
+						break;
+					}
+				}
+				ServerObj.Send((void*)&found, sizeof(int));
+				if (found==1)
+				{
+					ServerObj.Receive((void*)&temp, sizeof(StudentRecord));
+					file.seekp(sizeof(StudentRecord)*index);
+					cout << (int)file.tellp() << endl;
+					file.write((char*)&temp, sizeof(StudentRecord));
+				}
+				file.close();
+				break;
+			}
+		case 5:
+			{
+				file.open("Course_Outline.pdf", ios::in|ios::binary);
+				file.seekg(0, ios::end);
+				int filesize = (int)file.tellg();
+				ServerObj.Send((void*)&filesize, sizeof(int));
+
+				file.seekg(0, ios::beg);
+				char c;
+				for (int i=0; i<filesize; i++)
+				{
+					file.read((char*)&c, sizeof(char));
+					ServerObj.Send((void*)&c, sizeof(char));
+				}
+				file.close();
+				break;
+			}
+		case 6:
+			break;
+		}
+	}
+
+	// Close sockets.
+	ServerObj.CloseClientSocket();
+	ServerObj.CloseServerSocket();
+
+	return 0;
+}
