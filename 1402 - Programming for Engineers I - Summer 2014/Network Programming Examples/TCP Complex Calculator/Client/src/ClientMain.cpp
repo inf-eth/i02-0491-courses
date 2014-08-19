b34d8245c07070ea7f3a37219d305cab978f2bb6
infinity.ethereal <infinity.ethereal@9fa76b84-cec5-8abd-45b1-de927d2ca524>
2014-08-19 12:03:39 +0000

    [1402 - Programming for Engineers I - Summer 2014] - Renamed TCP Complex Calculator.

---
+++ 1402 - Programming for Engineers I - Summer 2014/Network Programming Examples/TCP Complex Calculator/Client/src/ClientMain.cpp
@@ -0,0 +1,124 @@
+#include <Client.h>
+#include <cstdlib>			// exit(), atoi()
+#include <cstring>			// strlen()
+#include <iostream>
+using namespace std;
+
+struct Complex
+{
+	float real;
+	float img;
+};
+void Input(Complex&);
+void Display(Complex&);
+
+int main()
+{
+	// Create Client object.
+	Client ClientObj;
+
+	// Create Client Socket.
+	ClientObj.CreateSocket(TCPSOCKET);
+	ClientObj.SetSocketOptions();
+
+	// Initialise and bind Client address.
+	ClientObj.InitialiseAddress(DEFAULTCLIENTPORT);	// Without any argument default port will be chosen.
+	ClientObj.Bind();
+
+	char ServerName[24];
+	int ServerPort;
+	cout << "Enter Server name or IP: "; // Use localhost or 127.0.0.1 for local server.
+	cin.getline(ServerName, 24);
+	cout << "Enter Server port: ";
+	cin >> ServerPort;
+
+	// Connect to Server. Server name/IP and port are provided as arguments.
+	ClientObj.Connect(ServerName, ServerPort);
+
+	// Send and receive.
+	Complex A, B, C;
+	int choice = -1;
+	char ReceiveBuffer[512];
+
+	while (choice != 5)
+	{
+		ClientObj.Receive((void*)ReceiveBuffer, 512);
+		cout << ReceiveBuffer << endl << ">> ";
+		cin >> choice;
+		ClientObj.Send((void*)&choice, sizeof(int));
+
+		switch (choice)
+		{
+			case 1:
+			{
+				cout << "Enter two complex numbers:" << endl;
+				Input(A);
+				Input(B);
+				ClientObj.Send((void*)&A, sizeof(Complex));
+				ClientObj.Send((void*)&B, sizeof(Complex));
+				ClientObj.Receive((void*)&C, sizeof(Complex));
+				cout << "Result is: ";
+				Display(C);
+				break;
+			}
+			case 2:
+			{
+				cout << "Enter two complex numbers:" << endl;
+				Input(A);
+				Input(B);
+				ClientObj.Send((void*)&A, sizeof(Complex));
+				ClientObj.Send((void*)&B, sizeof(Complex));
+				ClientObj.Receive((void*)&C, sizeof(Complex));
+				cout << "Result is: ";
+				Display(C);
+				break;
+			}
+			case 3:
+			{
+				cout << "Enter two complex numbers:" << endl;
+				Input(A);
+				Input(B);
+				ClientObj.Send((void*)&A, sizeof(Complex));
+				ClientObj.Send((void*)&B, sizeof(Complex));
+				ClientObj.Receive((void*)&C, sizeof(Complex));
+				cout << "Result is: ";
+				Display(C);
+				break;
+			}
+			case 4:
+			{
+				cout << "Enter two complex numbers:" << endl;
+				Input(A);
+				Input(B);
+				ClientObj.Send((void*)&A, sizeof(Complex));
+				ClientObj.Send((void*)&B, sizeof(Complex));
+				ClientObj.Receive((void*)&C, sizeof(Complex));
+				cout << "Result is: ";
+				Display(C);
+				break;
+			}
+			case 5:
+				break;
+		}
+	}
+
+	ClientObj.CloseClientSocket();
+
+	return 0;
+}
+
+void Input(Complex& C)
+{
+	cout << "Enter real: ";
+	cin >> C.real;
+	cout << "Enter img: ";
+	cin >> C.img;
+}
+
+void Display(Complex& C)
+{
+	if (C.img < 0.f)
+		cout << C.real << "-j" << -1*C.img << endl;
+	else
+		cout << C.real << "+j" << C.img << endl;
+}
---
+++ 1402 - Programming for Engineers I - Summer 2014/Network Programming Examples/TCP Complex Calculator/Server/src/ServerMain.cpp
@@ -0,0 +1,100 @@
+#include <Server.h>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+struct Complex
+{
+	float real;
+	float img;
+};
+
+int main()
+{
+	// Create Server object.
+	Server ServerObj;
+
+	// Create Server socket and set socket options.
+	ServerObj.CreateSocket(TCPSOCKET);		// No argument means TCPSOCKET
+	ServerObj.SetSocketOptions();
+
+	// Initialise Server address struct and bind it with Server's socket.
+	ServerObj.InitialiseAddress(6000);		// No argument here will take default port.
+	ServerObj.Bind();
+
+	// Listen for incoming connections.
+	ServerObj.Listen();
+	ServerObj.DisplayServerInfo();
+
+	// Accept any incoming connections.
+	ServerObj.Accept();
+	ServerObj.DisplayClientInfo();
+
+	// Send and receive.
+	int choice = -1;
+	Complex A, B, C;
+	char Menu[] = "1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5. Disconnect";
+
+	while (choice != 5)
+	{
+		ServerObj.Send((void*)Menu, strlen(Menu)+1);
+		ServerObj.Receive((void*)&choice, sizeof(int));
+
+		switch (choice)
+		{
+			case 1:
+			{
+				ServerObj.Receive((void*)&A, sizeof(Complex));
+				ServerObj.Receive((void*)&B, sizeof(Complex));
+				C.real = A.real + B.real;
+				C.img = A.img + B.img;
+				ServerObj.Send((void*)&C, sizeof(Complex));
+				break;
+			}
+			case 2:
+			{
+				ServerObj.Receive((void*)&A, sizeof(Complex));
+				ServerObj.Receive((void*)&B, sizeof(Complex));
+				C.real = A.real - B.real;
+				C.img = A.img - B.img;
+				ServerObj.Send((void*)&C, sizeof(Complex));
+				break;
+			}
+			case 3:
+			{
+				ServerObj.Receive((void*)&A, sizeof(Complex));
+				ServerObj.Receive((void*)&B, sizeof(Complex));
+				float a = A.real;
+				float b = A.img;
+				float c = B.real;
+				float d = B.img;
+				C.real = a*c-b*d;
+				C.img = a*d+b*c;
+				ServerObj.Send((void*)&C, sizeof(Complex));
+				break;
+			}
+			case 4:
+			{
+				ServerObj.Receive((void*)&A, sizeof(Complex));
+				ServerObj.Receive((void*)&B, sizeof(Complex));
+				float a = A.real;
+				float b = A.img;
+				float c = B.real;
+				float d = B.img;
+				float den =c*c+d*d;
+				C.real = (a*c+b*d)/den;
+				C.img = (b*c-a*d)/den;
+				ServerObj.Send((void*)&C, sizeof(Complex));
+				break;
+			}
+			case 5:
+				break;
+		}
+	}
+
+	// Close sockets.
+	ServerObj.CloseClientSocket();
+	ServerObj.CloseServerSocket();
+
+	return 0;
+}
