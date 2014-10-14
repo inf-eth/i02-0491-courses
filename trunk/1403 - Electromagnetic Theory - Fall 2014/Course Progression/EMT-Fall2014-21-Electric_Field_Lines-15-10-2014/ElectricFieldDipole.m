2e58f13873ff9fa96d89933c1d95c5f432b3705c
infinity.ethereal <infinity.ethereal@9fa76b84-cec5-8abd-45b1-de927d2ca524>
2014-10-14 23:43:42 +0000

    [1403 - Electromagnetic Theory - Fall 2014] - Added class 20 and 21.

---
+++ trunk/1403 - Electromagnetic Theory - Fall 2014/Course Progression/EMT-Fall2014-21-Electric_Field_Lines-15-10-2014/ElectricFieldDipole.m
@@ -0,0 +1,33 @@
+% Electric field of dipole.
+xMinRange=-2;
+yMinRange=-2;
+xMaxRange=2;
+yMaxRange=2;
+dx=0.125;
+dy=0.125;
+[x y] = meshgrid(xMinRange:dx:xMaxRange,yMinRange:dy:yMaxRange);
+
+rp = [-0.5 0];
+rn = [0.5 0];
+%r = [x y];
+qp = 1e-9;
+qn = -1e-9;
+e0 = (1e-9)/(36*pi);
+Epx = qp/(4*pi*e0)*(x-rp(1))./((x-rp(1)).^2+(y-rp(2)).^2).^3;
+Epy = qp/(4*pi*e0)*(y-rp(2))./((x-rp(1)).^2+(y-rp(2)).^2).^3;
+
+Enx = qn/(4*pi*e0)*(x-rn(1))./((x-rn(1)).^2+(y-rn(2)).^2).^3;
+Eny = qn/(4*pi*e0)*(y-rn(2))./((x-rn(1)).^2+(y-rn(2)).^2).^3;
+
+quiver(x,y,Epx+Enx,Epy+Eny)
+hold on
+r = 0.3;
+phi = 0:3.14/12:2*3.14;
+startx = r*cos(phi)+rp(1);
+starty = r*sin(phi)+rp(2);
+streamline(x,y,Epx+Enx,Epy+Eny,startx,starty)
+r = xMaxRange;
+phi = -3.14/2:3.14/12:3.14/2;
+startx = r*cos(phi)+rn(1);
+starty = r*sin(phi)+rn(2);
+streamline(x,y,Epx+Enx,Epy+Eny,startx,starty)
